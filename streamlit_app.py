5630a0319d9ed7067332c7a23d446da493ae8aa3
Sanchit Tomar <85986502+UnpretentiousGeek@users.noreply.github.com>
2024-09-02 16:55:40 +0000

    First Change

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,12 +12,13 @@ st.write(
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
 openai_api_key = st.text_input("OpenAI API Key", type="password")
+client = OpenAI(api_key=openai_api_key)
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
 
     # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+    
 
     # Let the user upload a file via `st.file_uploader`.
     uploaded_file = st.file_uploader(
