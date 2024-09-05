5c29395766fb7d357d94e9fde43a89834f2fa67d
Sanchit Tomar <85986502+UnpretentiousGeek@users.noreply.github.com>
2024-09-05 14:48:14 +0000

    10th

--- Lab_2.py
+++ Lab_2.py
@@ -37,21 +37,21 @@ disabled=not uploaded_file,
 
 if uploaded_file and question:
 
-# Process the uploaded file and question.
-document = uploaded_file.read().decode()
-messages = [
-    {
-        "role": "user",
-        "content": f"Here's a document: {document} \n\n---\n\n {question}",
-    }
-]
+    # Process the uploaded file and question.
+    document = uploaded_file.read().decode()
+    messages = [
+        {
+            "role": "user",
+            "content": f"Here's a document: {document} \n\n---\n\n {question}",
+        }
+    ]
 
-# Generate an answer using the OpenAI API.
-stream = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=messages,
-    stream=True,
-)
+    # Generate an answer using the OpenAI API.
+    stream = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=messages,
+        stream=True,
+    )
 
 # Stream the response to the app using `st.write_stream`.
 st.write_stream(stream)
