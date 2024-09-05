992bf7aba0e0173b764999e2a907088e2de003a1
Sanchit Tomar <85986502+UnpretentiousGeek@users.noreply.github.com>
2024-09-05 14:20:51 +0000

    3rd

--- Lab_2.py
+++ Lab_2.py
@@ -2,5 +2,6 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Lab 2",
-    layout="wide"
+    layout="wide",
+    initial_sidebar_state="expanded"
 )
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 Lab_1 = st.Page("Lab_1.py", title = "Lab 1")
-Lab_2 = st.Page("Lab_2.py", title = "Lab 1")
+Lab_2 = st.Page("Lab_2.py", title = "Lab 2")
 
 pg = st.navigation([Lab_2, Lab_1])
 st.set_page_config(page_title="Labs")
