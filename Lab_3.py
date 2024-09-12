207d64010b0058799a2038ec79de0b0cc907ff0e
Sanchit Tomar <85986502+UnpretentiousGeek@users.noreply.github.com>
2024-09-12 15:44:20 +0000

    37

--- Lab_3.py
+++ Lab_3.py
@@ -40,3 +40,6 @@ if prompt := st.chat_input("What is up?"):
         response = st.write_stream(stream)
 
     st.session_state.messages.append({"role": "assistant", "content": response})
+
+    if len(st.session_state.messages) > 5:
+        st.session_state.messages = st.session_state.messages[-5:]
