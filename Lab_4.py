1fc30cc7fedaa6abc030d93e8bbae2c64446310b
Sanchit Tomar <85986502+UnpretentiousGeek@users.noreply.github.com>
2024-09-19 16:15:16 +0000

    56h6

--- Lab_4.py
+++ Lab_4.py
@@ -63,7 +63,7 @@ def read_pdf(pdf_path):
 
 pdf_texts = {}
 for file_name in os.listdir('/pdfs'):
-    file_path = os.path.join('pdfs', file_name)
+    file_path = os.path.join('/pdfs', file_name)
     pdf_texts[file_name] = read_pdf(file_path)
     add_coll(st.session_state.Lab4_vectorDB, pdf_texts[file_name], file_name, st.session_state.openai_client)
 
