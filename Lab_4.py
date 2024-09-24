4433f801183df17a0c1a2eecde02b4b1c8f0998a
Sanchit Tomar <85986502+UnpretentiousGeek@users.noreply.github.com>
2024-09-24 03:30:45 +0000

    tfhyntyu

--- Lab_4.py
+++ Lab_4.py
@@ -54,7 +54,6 @@ if st.sidebar.button("Re-Scan"):
         file_path = os.path.join('pdfs', file_name)
         pdf_texts[file_name] = read_pdf(file_path)
         add_coll(st.session_state.Lab4_vectorDB, pdf_texts[file_name], file_name, st.session_state.openai_client)
-    st.write(f"The Collection have {st.session_state.Lab4_vectorDB.count()} file/syllabus might be helpful")
 
 openai_client = st.session_state.openai_client
 response = openai_client.embeddings.create(
