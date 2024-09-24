5a39c07069eb17ad5408e09a1bc2f70e43c23252
Sanchit Tomar <85986502+UnpretentiousGeek@users.noreply.github.com>
2024-09-24 03:32:19 +0000

    trbftybtf

--- Lab_4.py
+++ Lab_4.py
@@ -54,7 +54,7 @@ if st.sidebar.button("Re-Scan"):
         file_path = os.path.join('pdfs', file_name)
         pdf_texts[file_name] = read_pdf(file_path)
         add_coll(st.session_state.Lab4_vectorDB, pdf_texts[file_name], file_name, st.session_state.openai_client)
-        st.write(f"The Collection have {chroma_client.get_or_create_collection('Lab4Collection').count()} files.")
+    st.write(f"The Collection have {chroma_client.get_or_create_collection('Lab4Collection').count()} files.")
 
 openai_client = st.session_state.openai_client
 response = openai_client.embeddings.create(
