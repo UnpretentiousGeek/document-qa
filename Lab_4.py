a3ddb0289246659adcc98e2268d06177ce490b67
Sanchit Tomar <85986502+UnpretentiousGeek@users.noreply.github.com>
2024-09-24 03:10:29 +0000

    ertge

--- Lab_4.py
+++ Lab_4.py
@@ -44,13 +44,9 @@ def read_pdf(pdf_path):
         text += page.extract_text()
     return text
 
-st.write(f"The Collection have {st.session_state.Lab4_vectorDB.count()} files. Do you want to add more files?")
-if st.button("Yes"):
-    uploaded_file = st.file_uploader(
-        "Upload a document (.pdf)", type=("pdf")
-    )
-    if uploaded_file:
-        add_coll(st.session_state.Lab4_vectorDB, read_pdf(uploaded_file), uploaded_file, st.session_state.openai_client)
+
+topic = st.sidebar.selectbox("Topic", ("Generative AI", "Text Mining", "Data Science Overview"))
+
 
 if st.sidebar.button("Re-Scan"):
     pdf_texts = {}
@@ -59,24 +55,32 @@ if st.sidebar.button("Re-Scan"):
         pdf_texts[file_name] = read_pdf(file_path)
         add_coll(st.session_state.Lab4_vectorDB, pdf_texts[file_name], file_name, st.session_state.openai_client)
     st.write(f"The Collection have {st.session_state.Lab4_vectorDB.count()} file/syllabus might be helpful")
-    
-topic = st.sidebar.selectbox("Topic", ("Generative AI", "Text Mining", "Data Science Overview"))
 
 
-openai_client = st.session_state.openai_client
-response = openai_client.embeddings.create(
-    input = topic,
-    model = "text-embedding-3-small"
-)
+st.write(f"The Collection have {st.session_state.Lab4_vectorDB.count()} files. Do you want to add more files?")
+if st.button("Yes"):
+    uploaded_file = st.file_uploader(
+        "Upload a document (.pdf)", type=("pdf")
+    )
+    if uploaded_file:
+        add_coll(st.session_state.Lab4_vectorDB, read_pdf(uploaded_file), uploaded_file, st.session_state.openai_client)
+else:
 
-query_embedding = response.data[0].embedding
 
-result = st.session_state.Lab4_vectorDB.query(
-    query_embeddings = [query_embedding],
-    n_results=3
-)
+    openai_client = st.session_state.openai_client
+    response = openai_client.embeddings.create(
+        input = topic,
+        model = "text-embedding-3-small"
+    )
 
-for i in range(len(result['documents'][0])):
-    doc = result['documents'][0][i]
-    doc_id = result['ids'][0][i]
-    st.write(f"The following file/syllabus might be helpful: {doc_id}")
+    query_embedding = response.data[0].embedding
+
+    result = st.session_state.Lab4_vectorDB.query(
+        query_embeddings = [query_embedding],
+        n_results=3
+    )
+
+    for i in range(len(result['documents'][0])):
+        doc = result['documents'][0][i]
+        doc_id = result['ids'][0][i]
+        st.write(f"The following file/syllabus might be helpful: {doc_id}")
