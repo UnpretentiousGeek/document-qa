6739512193010b20d71993bca9014a3643ac00ad
Sanchit Tomar <85986502+UnpretentiousGeek@users.noreply.github.com>
2024-09-25 01:15:40 +0000

    fghnt

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,9 @@ Lab_1 = st.Page("Lab_1.py", title = "Lab 1")
 Lab_2 = st.Page("Lab_2.py", title = "Lab 2")
 Lab_3 = st.Page("Lab_3.py", title = "Lab 3")
 Lab_4 = st.Page("Lab_4.py", title = "Lab 4")
+Lab_test = st.Page("Lab_test.py", title = "Lab test")
 
-pg = st.navigation([Lab_4, Lab_3, Lab_2, Lab_1])
+pg = st.navigation([Lab_test, Lab_4, Lab_3, Lab_2, Lab_1])
 st.set_page_config(page_title="Labs")
 
 pg.run()
