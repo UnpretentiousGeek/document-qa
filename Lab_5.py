ff05d7f8bcaa59be48b0c94a9ebeea5a6e467b7e
Sanchit Tomar <85986502+UnpretentiousGeek@users.noreply.github.com>
2024-09-30 19:38:15 +0000

    srge

--- Lab_5.py
+++ Lab_5.py
@@ -109,6 +109,7 @@ if prompt := st.chat_input("Ask about weather"):
         tool_choice="auto",
     )
     response_message = response.choices[0].message
+    st.write(response_message)
     tool_calls = response_message.tool_calls
     if tool_calls:
         
